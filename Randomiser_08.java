950127331274452d2e2848e3907c483075bc9664
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-10-18 12:52:40 +0300

    Create Randomiser_08.java

---
+++ Randomiser_08.java
@@ -0,0 +1,23 @@
+package stefany;
+
+import java.util.Scanner;
+
+public class Randomiser_08 {
+
+	public static void main(String[] args) {
+  int[] array = new int[10];
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < 10; i++) {
+        array[i] = sc.nextInt();
+     }
+     for( int i = 0; i <10; i++) {
+         int randomIndex = (int) (Math.random() * 10);
+         int swap = array[randomIndex];
+         array[randomIndev] = array[i];
+         array[i] = swap;
+      }
+      for(int i = 0; i < 10; i++) {
+           System.out.println(array[i]);
+      }
+   }
+}
