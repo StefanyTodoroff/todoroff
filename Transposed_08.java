39ca14335ba8e23fd5f00cbc6eff95a8d8ec6e41
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-10-18 14:56:26 +0300

    Create Transposed_08.java

---
+++ Transposed_08.java
@@ -0,0 +1,47 @@
+package stefany;
+import java.util.Scanner;
+
+public class Transposed {
+
+
+
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			System.out.println("How many rows?");
+			int rowSize = sc.nextInt();
+			System.out.println("How many columns?");
+			int columnSize = sc.nextInt();
+			int[][] matrix = new int[rowSize][columnSize];
+			int[][] transposed = new int[columnSize][rowSize];
+			for(int i = 0; i < rowSize; i++) {
+				for(int j = 0; j < columnSize; j++) {
+					System.out.println("Enter a number on index " + i + ", " + j);
+					matrix[i][j] = sc.nextInt();
+				}
+			}
+			for(int i = 0; i < rowSize; i++) {
+				for(int j = 0; j < columnSize; j++) {
+					System.out.print( matrix[i][j] + "\t");
+				}
+				System.out.println();
+			}
+			for(int i = 0; i < rowSize; i++) {
+				for(int j = 0; j < columnSize; j++) {
+			transposed[j][i] = matrix[i][j];
+				}
+			}
+				System.out.println("Transposed: \n" );
+				
+				for(int i = 0; i < columnSize; i++) {
+					for(int j = 0; j < rowSize; j++) {
+						System.out.print( transposed[i][j] + "\t");
+					}
+					System.out.println();
+			}
+			
+		}
+
+	}
