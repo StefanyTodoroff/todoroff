f3b2004ac67508349909b992e5394f04c1e19530
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-11-05 15:24:15 +0200

    Create AccountsTest.java

---
+++ AccountsTest.java
@@ -0,0 +1,13 @@
+package stefany;
+
+public class AccountsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SavingsAccount stefy = new SavingsAccount();
+		stefy.deposit(1000);
+		stefy.withdraw(200);
+
+	}
+
+}
