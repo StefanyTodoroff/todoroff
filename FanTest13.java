9b9b04f9d9755dc893979bad7a84b6269b0b7caa
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2020-11-26 13:00:47 +0200

    Create FanTest13.java

---
+++ FanTest13.java
@@ -0,0 +1,25 @@
+package stefany;
+
+public class FanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fan f1 = new Fan();
+		f1.setSpeed(Fan.FAST);
+		f1.setRadius(10);
+		f1.setColor("yellow");
+		f1.setSwitchedOn(true);
+		System.out.println("First fan:");
+		System.out.println(f1.toString(f1.getSpeed(), f1.getRadius(), f1.getSwitchedOn(), f1.getColor()));
+		
+		Fan f2 = new Fan();
+		f2.setSpeed(Fan.MEDIUM);
+		f2.setRadius(5);
+		f2.setColor("blue");
+		f2.setSwitchedOn(false);
+		System.out.println("Second fan:");
+		System.out.println(f2.toString(f2.getSpeed(), f2.getRadius(), f2.getSwitchedOn(), f2.getColor()));
+		
+	}
+
+}
